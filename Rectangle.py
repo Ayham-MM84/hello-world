ecd23ee2e94d458c7790356752bb42c9b54e0dbb
Ayham-MM84 <153164222+Ayham-MM84@users.noreply.github.com>
2025-04-22 18:57:00 +0300

    Update Rectangle.py

--- Rectangle.py
+++ Rectangle.py
@@ -1,11 +1,11 @@
-LONG=15
+LENGTH=15
 INDENT=38
 left_I = INDENT-1
 right_I = INDENT
 print(" "*INDENT, "*", sep='') #Top of triangle
-for i in range(LONG-1): #Long of triangle side
+for i in range(LENGTH): #Length of triangle side
     print(" "*left_I, "*", " "*(right_I - left_I), "*", sep='')
     right_I+= 1
     left_I -= 1
-#Triangle base (i==LONG)
+#Triangle base (i==LENGTH)
 print(" "*left_I, "*"*(right_I - left_I + 2), sep='')
